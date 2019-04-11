0936d4a6a9110c7c7e397d1a36730426bc208885
Dani Lyn <dlyn1110@gmail.com>
2019-04-11 14:06:34 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,12 @@ class PostsController < ApplicationController
   end
 
   def show
-  end
+     @post = Post.find(params[:id])
+     respond_to do |format|
+       format.html { render :show }
+       format.json { render json: @post.to_json(only: [:title, :description, :id],
+                               include: [author: { only: [:name]}]) }
+     end
 
   def new
     @post = Post.new
